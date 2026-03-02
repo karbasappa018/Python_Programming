e022ed34f075f144462a82e99344d4dff9fa4950
karbasappa018 <karbasappatadakale@gmail.com>
2026-03-02 23:39:37 +0530

    Add Python example demonstrating List (mutable) and Tuple (immutable) with indexing and modification

---
+++ Conceptual/program3.py
@@ -0,0 +1,23 @@
+# List
+# mutable
+# accessing the element by index is allowed
+Arr = [10,20,30,40,10]
+print(Arr)
+print(type(Arr))
+
+# Modifying the element in the list is allowed
+Arr[3]= 23
+print(Arr)
+
+# tuple
+# immutable
+# accessing the element by index is allowed
+
+# Modifying the elements in the tuple is not allowed
+
+Brr = (10,20,30,40,10)
+print(Brr)
+print(type(Brr))
+
+print("_"*15) # prints 15 times "_______________".
+
