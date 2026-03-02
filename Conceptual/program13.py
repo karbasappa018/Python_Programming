677568a227489985f5b22cf5be5e97c3f51be10d
karbasappa018 <karbasappatadakale@gmail.com>
2026-03-02 23:46:05 +0530

    Add Python memory size example using sys module

---
+++ Conceptual/program13.py
@@ -0,0 +1,11 @@
+import sys
+
+No =12345234535567777556546465565666466466
+
+Marks = 90.90  #float
+Name = "Rahul" #str
+
+
+print(sys.getsizeof(No))
+print(sys.getsizeof(Marks))
+print(sys.getsizeof(Name))
