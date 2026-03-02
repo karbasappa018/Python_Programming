4ce262c245b8560b8be0d4db65bfd734464691a8
karbasappa018 <karbasappatadakale@gmail.com>
2026-03-02 23:46:43 +0530

    Add Python input() example

---
+++ Conceptual/program14.py
@@ -0,0 +1,17 @@
+print("Enter you name:")
+Name = input()
+
+print("Hello",Name)
+
+print("Enter your age:")
+Age = input()
+
+print("Your age is:",Age)
+
+#another way
+
+Name = input("Enter your Name:")
+print("Hello",Name)
+
+Age = input("Enter your age:")
+print("your age is:",Age)
