4e8396d3e5b0ae0d2a473ffd10ee2f28ce861300
karbasappa018 <karbasappatadakale@gmail.com>
2026-03-02 23:58:20 +0530

    Add Python function example for multiplication

---
+++ Conceptual/program20.py
@@ -0,0 +1,11 @@
+def Multiplication(Value1, Value2):
+    Ans = Value1 * Value2
+    return Ans
+
+print("Demo Application")
+
+No1 = 10
+No2 = 11
+Reasult = 0
+Reasult = Multiplication(No1, No2)
+print("Multiplication is :",Reasult)
